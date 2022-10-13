c061606a01267b2337517f63ff8133a581730ef0
AndrezJimenez <andru456@hotmail.com>
2022-10-12 21:29:05 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,57 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'category',
+    component: CategoryComponent
+  },
+  {
+    path: 'recovery',
+    component: RecoveryComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'notfound',
+    component: NotFoundComponent
+  },
+
+  {
+    path: 'mycart',
+    component: MycartComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
