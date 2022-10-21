ebb7a1d9c01540c3b5ebd83d94b25012b531c757
AndrezJimenez <andru456@hotmail.com>
2022-10-20 22:22:40 -0500

    RouterLink and RouterActive

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { StoreService } from '../../services/store.service'
 import { AuthService } from '../../services/auth.service';
+import { CategoriesService } from '../../services/categories.service';
 import { User } from '../../models/user.model';
+
+import { Category } from '../../models/category.model';
+
 
 @Component({
   selector: 'app-nav',
@@ -14,16 +18,18 @@ export class NavComponent implements OnInit {
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
-
+  categories : Category [] = [];
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
+    private categoriesService: CategoriesService
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -35,6 +41,13 @@ export class NavComponent implements OnInit {
     .subscribe(user => {
       this.profile = user;
     });
+  }
+
+  getAllCategories () {
+    this.categoriesService.getAll()
+    .subscribe ( data => {
+      this.categories = data;
+    })
   }
 
 }
